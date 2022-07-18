d8ac8faf145fdaac815885f001f675257eebbbff
mkskopaing123 <102409234+mkskopaing123@users.noreply.github.com>
2022-07-18 22:26:42 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,55 +1,50 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @trojanzhex
+import logging
+import logging.config
 
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
-from pyrogram import (
-    Client,
-    __version__
-)
-
-from config import (
-    API_HASH,
-    APP_ID,
-    LOGGER,
-    AUTH_USERS,
-    TG_BOT_SESSION,
-    TG_BOT_TOKEN,
-    TG_BOT_WORKERS
-)
-
-from user import User
-
-
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from utils import temp
 
 class Bot(Client):
-    USER: User = None
-    USER_ID: int = None
 
     def __init__(self):
         super().__init__(
-            TG_BOT_SESSION,
+            session_name=SESSION,
+            api_id=API_ID,
             api_hash=API_HASH,
-            api_id=APP_ID,
-            plugins={
-                "root": "plugins"
-            },
-            workers=TG_BOT_WORKERS,
-            bot_token=TG_BOT_TOKEN
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
         )
-        self.LOGGER = LOGGER
 
     async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
         await super().start()
-        usr_bot_me = await self.get_me()
-        self.set_parse_mode("html")
-        self.LOGGER(__name__).info(
-            f"@{usr_bot_me.username}  started!\n\n"
-            f"Add @{usr_bot_me.username} as admin with all rights in your required channels\n\n"
-        )
-        AUTH_USERS.add(680815375)
-        self.USER, self.USER_ID = await User().start()
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
+        logging.info("Bot stopped. Bye.")
+
+
+app = Bot()
+app.run()
